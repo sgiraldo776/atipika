ff069751cc9e3ba0eeb5d3a948c31250bb1b4918
sam-oro <samiroroz@gmail.com>
2021-01-04 17:45:52 -0500

    Estilos

--- vistas/estilos/estilos.php
+++ vistas/estilos/estilos.php
@@ -28,7 +28,8 @@
     <title>Estilos</title>
 
     <!--importacion boostrap-->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+        integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
     <!-- Google Fonts -->
     <link rel="preconnect" href="https://fonts.gstatic.com">
@@ -48,32 +49,42 @@
             <?php
                 while($fila=$sel->fetch_assoc()){
             ?>
-            <div class="card" style="width: 18rem;">
-                <img src="<?php echo $urlimagen.$fila['imagen']; ?>" class="card-img-top">
+
+            <div class="card m-3" style="width: 15rem;">
+                <img src="<?php echo $urlimagen.$fila['imagen'] ?>" class="imagen-portada card-img-top"
+                    style="height: 15rem;" />
                 <div class="card-body">
-                    <div class="col-12 text-center">
-                        <h5 class="card-title"><?php echo $fila['nombre']; ?></h5>
-                    </div>
+                    <h5 class="card-title text-center">
+                        <?php echo $fila['nombre'] ?>
+                    </h5>
+                    <!-- <p class="card-text">
+                                            This is a longer card with supporting text below as a natural lead-in to
+                                            additional content. This content is a little bit longer.
+                                        </p> -->
 
                     <div class="align-items-center text-center mt-2">
                         <button type="submit" class="btn-color">Comprar</button>
                         <button type="submit" class="btn-color">Agregar a la bolsa</button>
                     </div>
-
                 </div>
             </div>
+
+
             <?php
                 }
             ?>
         </div>
         <a href="../crear-estilo/crear_estilo.php">Crea tu propia prenda aquí</a>
     </div>
-    
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous">
+
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
+        integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous">
     </script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
+        integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous">
     </script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
+        integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous">
     </script>
 </body>
 
