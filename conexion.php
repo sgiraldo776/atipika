6100f90ef36b581ea35d14571294c2a1d4db9b60
Mauricio <maurox952@gmail.com>
2021-01-05 16:51:05 -0500

    conexion

--- conexion.php
+++ conexion.php
@@ -1,13 +1,13 @@
 <?php
 
 //Los otros jaja
-// $conn = new mysqli('localhost','root','','atipika');
+$conn = new mysqli('localhost','root','','atipika');
 
 $URL = "http://localhost/atipika/";
 $urlimagen="http://localhost/atipika/images/";
 
     //Samir
-$conn = new mysqli('localhost','root','','atipika','33065');
-$URL = "http://localhost:8080/atipika/";
-$urlimagen="http://localhost:8080/atipika/images/";
+//$conn = new mysqli('localhost','root','','atipika','33065');
+//$URL = "http://localhost:8080/atipika/";
+//$urlimagen="http://localhost:8080/atipika/images/";
 ?>
