8c7e2926a075fa9b101046efef664ef0e0b6cac5
sam-oro <samiroroz@gmail.com>
2021-01-29 14:48:51 -0500

    Correciones carrito de compras

--- carrito/ver_carrito.php
+++ carrito/ver_carrito.php
@@ -1,8 +1,6 @@
 <?php
     include '../conexion.php';
 
-    //include './payu/lib/PayU.php';
-
     session_start();
         if(!isset($_SESSION['rol'])){
             include '../../includes/header/header_inicio.php';
@@ -23,22 +21,7 @@
             LEFT JOIN `tbldiseñohechos` ON `cart_item`.`cod_diseño_hecho` = `tbldiseñohechos`.`cod_diseño_hecho` 
             LEFT JOIN `tblcliente` ON `cart_item`.`id` = `tblcliente`.`id` WHERE `cart_item`.`id` = $_SESSION[id]");
             $suma = 0;
-
-            //Environment::setPaymentsCustomUrl("https://api.payulatam.com/payments-api/4.0/service.cgi");
-
-                                //Ingrese aquí el nombre del medio de pago
-                    //$parameters = array(
-                        //Ingrese aquí el identificador de la cuenta.
-                        //PayUParameters::PAYMENT_METHOD => "PSE",
-                        //Ingrese aquí el nombre del pais.
-                        //PayUParameters::COUNTRY => PayUCountries::CO,
-                    //);
-                    //$array=PayUPayments::getPSEBanks($parameters);
-                    //$banks=$array->banks;
-
-                    
-
-?>
+        ?>
 
 <!DOCTYPE html>
 <html lang="es">
@@ -69,19 +52,19 @@
 </head>
 
 <body>
-        
 
-        <div class="container">
-                <h1 class="mt-4">Carrito de compras</h1>
-                <div class="mt-4">
-                    <table class="table table-hover">
-                        <thead class="thead">
-                            <th>Imagen</th>
-                            <th>Productos</th>
-                            <th>Cantidad</th>
-                            <th>Valor</th>
-                        </thead>
-                        <?php 
+
+    <div class="container">
+        <h1 class="mt-4">Carrito de compras</h1>
+        <div class="mt-4">
+            <table class="table table-hover">
+                <thead class="thead">
+                    <th>Imagen</th>
+                    <th>Productos</th>
+                    <th>Cantidad</th>
+                    <th>Valor</th>
+                </thead>
+                <?php 
 
                         $cont=0;
                         
@@ -93,21 +76,22 @@
 
                         
                         ?>
-                        <tr>
-                            <td><img style="width: 130px; height:150px;" src="<?php echo $urlimagen.$fila['imagen'] ?>" alt="<?php echo $fila['productos'] ?>"></td>
-                            <td><?php echo $fila[1] ?></td>
-                            <td><?php echo $fila[2] ?></td>
-                            <td><?php echo $fila[4] ?></td>
-                            <select name="" id="">
-                            <?php 
+                <tr>
+                    <td><img style="width: 130px; height:150px;" src="<?php echo $urlimagen.$fila['imagen'] ?>"
+                            alt="<?php echo $fila['productos'] ?>"></td>
+                    <td><?php echo $fila[1] ?></td>
+                    <td><?php echo $fila[2] ?></td>
+                    <td><?php echo $fila[4] ?></td>
+                    <select name="" id="">
+                        <?php 
                             foreach ($banks as $bank) {
                             ?>
-                            <option value=<?php echo $bank-> pseCode ?>><?php echo $bank->description  ?></option>
-                            <?php
+                        <option value=<?php echo $bank-> pseCode ?>><?php echo $bank->description  ?></option>
+                        <?php
                             } ?>
-                            
-                            </select>
-                            <?php
+
+                    </select>
+                    <?php
 
                             
 
@@ -115,9 +99,10 @@
 
                             ?>
 
-                            <td><a href="#" onclick="preguntar(<?php echo $fila['id_carrito']?>)"><button class="btn btn-color">Quitar</button></a></td>
-                        </tr>
-                        <?php
+                    <td><a href="#" onclick="preguntar(<?php echo $fila['id_carrito']?>)"><button
+                                class="btn btn-color">Quitar</button></a></td>
+                </tr>
+                <?php
                             $cont++;
                         }
                         if (isset($productos)){
@@ -127,71 +112,68 @@
                             $_SESSION['productos']=0;
                         }
                         ?>
-                    </table>
-                        
-                       <?php
+            </table>
 
-                       $merchantId="508029";
-                       $ApiKey="4Vj8eK4rloUd272L48hsrarnUA";
-                       $referenceCode=date("Y-m-d H:i:s");
-                       $amount=$suma;
-                       $currency="COP";
-                       $correo=$_SESSION['correo'];
+            <?php
 
-                       $acum=$ApiKey."~".$merchantId."~".$referenceCode."~".$amount."~".$currency;
+                $merchantId="508029";
+                $ApiKey="4Vj8eK4rloUd272L48hsrarnUA";
+                $referenceCode=date("Y-m-d H:i:s");
+                $amount=$suma;
+                $currency="COP";
+                $correo=$_SESSION['correo'];
 
-                       $j=md5($acum);
+                $acum=$ApiKey."~".$merchantId."~".$referenceCode."~".$amount."~".$currency;
 
-                       
+                $j=md5($acum);
 
-                       
-                       ?>
+                ?>
 
-                    
-                    <form method="post" action="https://sandbox.checkout.payulatam.com/ppp-web-gateway-payu">
-                    <input name="merchantId"    type="hidden"  value="<?php echo $merchantId ?>">
-                    <input name="ApiKey"    type="hidden"  value="<?php echo $ApiKey ?>">
-                    <input name="accountId"     type="hidden"  value="512321" >
-                    <input name="description"   type="hidden"  value="Test PAYU"  >
-                    <input name="referenceCode" type="hidden"  value="<?php echo $referenceCode ?>" >
-                    <input name="amount"        type="hidden"  value="<?php echo $amount ?>"   >
-                    <input name="tax"           type="hidden"  value="0"  >
-                    <input name="taxReturnBase" type="hidden"  value="0" >
-                    <input name="currency"      type="hidden"  value="<?php echo $currency ?>" >
-                    <input name="signature"     type="hidden"  value="<?php echo $j ?>" >
-                    <input name="test"          type="hidden"  value="1" >
-                    <input name="buyerEmail"    type="hidden"  value="<?php echo $correo ?>" >
-                    <input name="responseUrl"    type="hidden"  value="http://www.test.com/response" >
-                    <input name="confirmationUrl"    type="hidden"  value="http://www.test.com/confirmation" >
-                    <?php
+
+            <form method="post" action="https://sandbox.checkout.payulatam.com/ppp-web-gateway-payu">
+                <input name="merchantId" type="hidden" value="<?php echo $merchantId ?>">
+                <input name="ApiKey" type="hidden" value="<?php echo $ApiKey ?>">
+                <input name="accountId" type="hidden" value="512321">
+                <input name="description" type="hidden" value="Test PAYU">
+                <input name="referenceCode" type="hidden" value="<?php echo $referenceCode ?>">
+                <input name="amount" type="hidden" value="<?php echo $amount ?>">
+                <input name="tax" type="hidden" value="0">
+                <input name="taxReturnBase" type="hidden" value="0">
+                <input name="currency" type="hidden" value="<?php echo $currency ?>">
+                <input name="signature" type="hidden" value="<?php echo $j ?>">
+                <input name="test" type="hidden" value="1">
+                <input name="buyerEmail" type="hidden" value="<?php echo $correo ?>">
+                <input name="responseUrl" type="hidden" value="http://www.test.com/response">
+                <input name="confirmationUrl" type="hidden" value="http://www.test.com/confirmation">
+                <?php
 
                     if (!empty($suma)){
                         ?>
-                        <input name="Submit" class="btn btn-color" type="submit"  value="Enviar" >
-                        <div class="container">
-                        <h1><?php echo $suma ?></h1>
-                        </div>
-                    <?php	
+                <div class="col-12 d-flex" style=" float: right;">
+                    <div class="ml-auto">
+                        <h4>Total: <?php echo $suma ?></h4>
+                    </div>
+                </div>
+
+                <div class="text-center">
+                    <input name="Submit" class="btn btn-color" type="submit" value="Enviar">
+                </div>
+
+                <?php	
                     }else{
                         ?>
-                        <h1>carrito vacio sumerce</h1>
-                    <?php
+                <h3>El carrito de compras está vacío</h3>
+                <?php
                     }
                     ?>
-                    
-                    </form>
-                   
-                    <!-- <a class="btn btn-color" href="enviar_pedido.php">Confirmar pedido</a> -->
-                </div>
-            </div>
-        </div>
-            
-
-
-
+            </form>
+            <!-- <a class="btn btn-color" href="enviar_pedido.php">Confirmar pedido</a> -->
         </div>
     </div>
+    </div>
 
+    </div>
+    </div>
 
     <div class="my-5">
     </div>
@@ -199,28 +181,33 @@
     <footer class="footer py-4 fixed-bottom">
         <div class="container">
             <div class="row align-items-center">
-                <div class="col-lg-3 text-lg-left text-center copy">Atipika © <script>document.write(new Date().getFullYear());</script></div>
+                <div class="col-lg-3 text-lg-left text-center copy">Atipika © <script>
+                        document.write(new Date().getFullYear());
+                    </script>
+                </div>
                 <div class="col-lg-6 my-3 my-lg-0 text-lg-center text-center">
-                    <a class="redes btn btn-social mx-3" href="https://cutt.ly/gjg4kjS" target="_blank"><i class="fab fa-whatsapp"></i></a>
-                    <a class="redes btn btn-social mx-3" href="wa.link/vtu4ai" target="_blank"><i class="fab fa-facebook-f"></i></a>
+                    <a class="redes btn btn-social mx-3" href="https://cutt.ly/gjg4kjS" target="_blank"><i
+                            class="fab fa-whatsapp"></i></a>
+                    <a class="redes btn btn-social mx-3" href="wa.link/vtu4ai" target="_blank"><i
+                            class="fab fa-facebook-f"></i></a>
                     <a class="redes btn btn-social mx-3" href="https://www.instagram.com/atipika_oficial/?hl=es-la"
                         target="_blank"><i class="fab fa-instagram"></i></a>
                 </div>
                 <div class="col-lg-3 text-lg-center text-center contac">
-                    <h3 class="contac"><a href="<?php echo $URL ?>vistas/contactenos/contacto.php" >Contáctenos</a></h3>
+                    <h3 class="contac"><a href="<?php echo $URL ?>vistas/contactenos/contacto.php">Contáctenos</a></h3>
                 </div>
             </div>
         </div>
-    </footer> 
+    </footer>
 
     <!-- pregunta antes de eliminar sweat alert -->
     <script type="text/javascript">
-            function preguntar(id){
+        function preguntar(id) {
             Swal
                 .fire({
                     title: "¿Eliminar el diseño?",
                     text: "¿Estas seguro de eliminar el item del carrito?",
-                    icon: 'error',            
+                    icon: 'error',
                     showCancelButton: true,
                     confirmButtonText: "Sí, eliminar",
                     cancelButtonText: "Cancelar",
@@ -229,48 +216,46 @@
                     if (resultado.value) {
                         // Hicieron click en "Sí"
                         //console.log("*se elimina la venta*");
-                        window.location.href="eliminar_item.php?id_carrito="+id
+                        window.location.href = "eliminar_item.php?id_carrito=" + id
                     } else {
                         // Dijeron que no
-                        console.log("*NO se elimina*");
+                        console.log("*No se elimina*");
                     }
                 });
 
-            }
-        </script>
+        }
+    </script>
 
 
-        <?php
+    <?php
         if(isset($_GET['msg'])){
             if($_GET['msg']==1){
         ?>
 
-        <script>
-            Swal.fire({
-                icon: 'error',
-                title: 'Error...',
-                text: 'El carrito está vacio',
-            
-            })
-        </script>
+    <script>
+        Swal.fire({
+            icon: 'error',
+            title: 'Error...',
+            text: 'El carrito está vacio',
 
-        <?php
+        })
+    </script>
+
+    <?php
             }else{
                 if($_GET['msg']==2){
         ?>
 
-        <script>
-            Swal.fire('No se pudo agregar al carrito')
-        </script>
+    <script>
+        Swal.fire('No se pudo agregar al carrito')
+    </script>
 
-        <?php
+    <?php
                 }
             }
         }
         ?>
 
-
-    
 
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
         integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous">
